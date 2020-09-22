31d1f91dac6f7e9467f729999be0f8e03bad5329
José de Jesus Filho <jjesusfilho@gmail.com>
2020-09-22 10:30:53 -0300

    novas funções

--- R/trf4_baixar_cposg_id.R
+++ R/trf4_baixar_cposg_id.R
@@ -1,6 +1,6 @@
-#' Baixar julgados de segundo grau conforme id
+#' Baixar cposg conforme id
 #'
-#' @param id Id do processo. Pode ser o id completo ou o número.
+#' @param id Id do processo. Pode ser completo ou seomente os números.
 #' @param diretorio Diretório
 #'
 #' @details Há uma sequência de ids que podem ser baixados. Vc pode formá-la, se quiser.
@@ -8,8 +8,10 @@
 #' @return htmls
 #' @export
 #'
-trf4_baixar_cjsg_id <- function(id = NULL, diretorio = "."){
+trf4_baixar_cposg_id <- function(id = NULL, diretorio = "."){
 
+
+    pb <- progress::progress_bar$new(total = length(id))
 
   purrr::walk(id,purrr::possibly(~{
 
@@ -18,10 +20,11 @@ trf4_baixar_cjsg_id <- function(id = NULL, diretorio = "."){
 
     if (str_detect(.x,"TRF4",negate=TRUE)){
 
-      .x <- .x %>%
-        stringr::str_pad(8,"left","0") %>%
-        stringr::str_c("TRF4",.)
+    .x <- .x %>%
+      stringr::str_pad(8,"left","0") %>%
+      stringr::str_c("TRF4",.)
     }
+
 
     url <- "https://jurisprudencia.trf4.jus.br/pesquisa/resultado_pesquisa.php"
 
@@ -49,9 +52,17 @@ trf4_baixar_cjsg_id <- function(id = NULL, diretorio = "."){
       hdnTipo = "1"
     )
 
-    arquivo <- file.path(diretorio,paste0(.x,".html"))
 
-    httr::POST(url,body= body, encode="form",httr::write_disk(arquivo,overwrite = TRUE))
+  url1 <-  httr::POST(url,body= body, encode="form") %>%
+         httr:content() %>%
+    xml2::xml_find_first("//td[contains(b,'Processo:')]/a") %>%
+    xml2::xml_attr("href")
+
+
+
+    arquivo <- file.path(diretorio,paste0("cposg_",.x,".html"))
+
+    httr::GET(url1, httr::write_disk(arquivo,overwrite = TRUE))
 
 
   },NULL))
--- R/trf4_ler_partes_cposg.R
+++ R/trf4_ler_partes_cposg.R
@@ -6,42 +6,29 @@
 #' @return tibble
 #' @export
 #'
-trf4_ler_partes_cposg <- function(arquivos = NULL, diretorio = "."){
-
-  if (is.null(arquivos)){
-
-    arquivos <- list.files(diretorio, pattern = "html$", full.names = TRUE)
-
+trf4_ler_partes_cposg <- function (arquivos = NULL, diretorio = ".")
+{
+  if (is.null(arquivos)) {
+    arquivos <- list.files(diretorio, pattern = "html$",
+                           full.names = TRUE)
   }
-
   pb <- progress::progress_bar$new(total = length(arquivos))
-
   purrr::map_dfr(arquivos, purrr::possibly(~{
-
     pb$tick()
-
-    id <- stringr::str_extract(.x,"TRF4\\d+")
-
-    x <- xml2::read_html("data-raw/a.html")
-
-    parte_nome <- x %>%
-      xml2::xml_find_all("//div//a[contains(text(),'todas as partes')]") %>%
+    id <- stringr::str_extract(.x, "TRF4\\d+")
+    x <- xml2::read_html(.x)
+    parte_nome <- x %>% xml2::xml_find_all("//div//a[contains(text(),'todas as partes')]") %>%
       xml2::xml_find_all("./preceding-sibling::hr/following-sibling::text()") %>%
-      xml2::xml_text(trim = TRUE) %>%
-      stringr::str_subset("^[A-Z]{3,}") %>%
+      xml2::xml_text(trim = TRUE) %>% stringr::str_subset("^[A-Z]{3,}") %>%
       stringr::str_remove(":")
 
-
-
-    parte <- x %>%
-      xml2::xml_find_all("//div//a[contains(text(),'todas as partes')]") %>%
+    parte <- x %>% xml2::xml_find_all("//div//a[contains(text(),'todas as partes')]") %>%
       xml2::xml_find_all("./preceding-sibling::hr/following-sibling::text()/following-sibling::strong") %>%
       xml2::xml_text(trim = TRUE) %>%
       .[1:length(parte_nome)]
 
-    tibble::tibble(id = id, parte = parte, parte_nome = parte_nome)
 
-
-  },NULL))
-
+    tibble::tibble(id = id, parte = parte, parte_nome = parte_nome) %>%
+      dplyr::filter(stringr::str_detect(parte,"^\\d+/",negate=TRUE))
+  }, NULL))
 }
