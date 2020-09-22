60b2b6422483610a6ea09bb49f3342a9037d0f52
José de Jesus Filho <jjesusfilho@gmail.com>
2020-09-22 10:42:53 -0300

    novas funções

--- R/trf4_baixar_cjsg_id.R
+++ R/trf4_baixar_cjsg_id.R
@@ -8,52 +8,33 @@
 #' @return htmls
 #' @export
 #'
-trf4_baixar_cjsg_id <- function(id = NULL, diretorio = "."){
+trf4_baixar_cjsg_id <- function (id = NULL, diretorio = ".")
+{
 
+  pb <- progress::progress_bar$new(total = length(id))
 
-  purrr::walk(id,purrr::possibly(~{
-
+  purrr::walk(id, purrr::possibly(~{
 
     pb$tick()
 
-    if (stringr::str_detect(.x,"TRF4",negate=TRUE)){
-
-      .x <- .x %>%
-        stringr::str_pad(8,"left","0") %>%
-        stringr::str_c("TRF4",.)
+    if (stringr::str_detect(.x, "TRF4", negate = TRUE)) {
+      .x <- .x %>% stringr::str_pad(8, "left", "0") %>%
+        stringr::str_c("TRF4", .)
     }
 
     url <- "https://jurisprudencia.trf4.jus.br/pesquisa/resultado_pesquisa.php"
 
-    body <- list(
-      parcial = "aqui",
-      pesquisaLivre = "",
-      registrosSelecionados = "",
-      vetPaginacao = .x,
-      paginaAtual = "0",
-      totalRegistros = "",
-      rdoCampoPesquisa = "I",
-      chkAcordaos = "on",
-      chkDecMono = "on",
-      textoPesqLivre = "",
-      chkDocumentosSelecionados = "",
-      numProcesso = "",
-      cboRelator = "",
-      dataIni = "",
-      dataFim = "",
-      tipodata = "DEC",
-      docsPagina = "",
-      arrorgaos = "",
-      arrclasses = "",
-      hdnAcao = "",
-      hdnTipo = "1"
-    )
+    body <- list(parcial = "aqui", pesquisaLivre = "", registrosSelecionados = "",
+                 vetPaginacao = .x, paginaAtual = "0", totalRegistros = "",
+                 rdoCampoPesquisa = "I", chkAcordaos = "on", chkDecMono = "on",
+                 textoPesqLivre = "", chkDocumentosSelecionados = "",
+                 numProcesso = "", cboRelator = "", dataIni = "",
+                 dataFim = "", tipodata = "DEC", docsPagina = "",
+                 arrorgaos = "", arrclasses = "", hdnAcao = "", hdnTipo = "1")
 
-    arquivo <- file.path(diretorio,paste0(.x,".html"))
+    arquivo <- file.path(diretorio, paste0("cjsg_",.x, ".html"))
 
-    httr::POST(url,body= body, encode="form",httr::write_disk(arquivo,overwrite = TRUE))
-
-
-  },NULL))
-
+    httr::POST(url, body = body, encode = "form", httr::write_disk(arquivo,
+                                                                   overwrite = TRUE))
+  }, NULL))
 }
