88d6ce5323d377f7137d3939ef285bc02159d6a2
José de Jesus Filho <jjesusfilho@gmail.com>
2020-12-28 07:37:52 -0300

    ajusta funcao trf4_ler_partes_docs_html

--- R/trf4_ler_partes_docs_html.R
+++ R/trf4_ler_partes_docs_html.R
@@ -6,36 +6,35 @@
 #' @return tibble
 #' @export
 #'
-trf4_ler_partes_docs_html <- function(arquivos = NULL, diretorio = "."){
+trf4_ler_partes_docs_html <- function (arquivos = NULL, diretorio = ".")
+{
 
-  if (is.null(arquivos)){
-
+  if (is.null(arquivos)) {
     arquivos <- list.files(diretorio, full.names = TRUE)
-
   }
 
   pb <- progress::progress_bar$new(total = length(arquivos))
 
-  purrr::map_dfr(arquivos,purrr::possibly(~{
+  purrr::map_dfr(arquivos, purrr::possibly(~{
 
     pb$tick()
 
-    id <- stringr::str_extract(.x,"TRF4\\d+")
+    id <- stringr::str_extract(.x, "TRF4\\d+")
 
     x <- .x %>%
       xml2::read_html()
 
-
     var <- x %>%
-      xml2::xml_find_all("//section[@data-nome='partes']//span[@class='tipo_parte']") %>%
+      xml2::xml_find_all("//section[@data-nome='partes']//span[@class='tipo_parte'] |//td[@width='22%']/div[@class='parte']") %>%
       xml2::xml_text()
 
     val <- x %>%
-      xml2::xml_find_all("//section[@data-nome='partes']//span[@class='nome_parte']") %>%
+      xml2::xml_find_all("//section[@data-nome='partes']//span[@class='nome_parte']|//td[@width='3%']/following-sibling::td/div[@class='parte']") %>%
       xml2::xml_text()
 
-    tibble::tibble(id, variavel = var, valor = val)
+    tibble::tibble(id, variavel = var, valor = val) %>%
+      dplyr::mutate(variavel = dplyr::na_if(variavel,"")) %>%
+      tidyr::fill(variavel)
 
-  },NULL))
-
+  }, NULL))
 }
